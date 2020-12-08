4108fd01fc9ef3c5f76b8d20d3ba05fe9bcd78ec
rashahameed <75690612+rashahameed@users.noreply.github.com>
2020-12-08 22:52:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function doBounce(element, times, distance, speed) {
+  for(var i = 0; i < times; i++) {
+      element.animate({marginTop: '-='+distance}, speed)
+             .animate({marginTop: '+='+distance}, speed);
+  }
+}
+
+function showMessage(id) {
+  var message = $('#message-' + id);
+  message.css('display', 'inline-block');
+  doBounce(message, 1, '4px', 150);
+}
+
+function start() {
+  var messages = $('.message');
+  var timeOut = 1000;
+
+  messages.each(function(index) {
+    index += 1;
+    if (index === messages.length) window.setTimeout( function() { writer.remove(); }, timeOut );
+    window.setTimeout( function() { showMessage(index); }, timeOut );
+    timeOut += 1000;
+  });
+}
+
+$(document).ready(function() {
+  start();
+  $('#replay').click(start());
+});
